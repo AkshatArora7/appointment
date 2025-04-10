58c957fca3a507e4536b46923149d441dbbc4208
Akshat Arora <akshat.arora456@gmail.com>
2025-04-10 16:19:24 -0400

    deployed version

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
-const { i18n } = require('./next-i18next.config');
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  i18n,
   eslint: {
     // This will disable ESLint checking during builds
     ignoreDuringBuilds: true,
